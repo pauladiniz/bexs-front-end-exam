cfe094b26ac667cbf8d1eec83b4d39da4b9227dd
Paula Diniz <bomdiapaulaa@gmail.com>
2020-10-14 16:50:02 -0300

    Adding main components files.

---
+++ components/AppHeader.vue
@@ -0,0 +1,11 @@
+<template>
+  <header class="app-header">
+    DEMO SHOP
+  </header>
+</template>
+
+<style lang="stylus">
+  .app-header
+    border-bottom 1px solid rgb(232, 232, 232)
+    height 80px
+</style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'bexs-front-end-exam',
+    title: 'Bexs Front End Exam',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
